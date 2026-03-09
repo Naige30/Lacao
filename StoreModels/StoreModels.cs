864d37a24a1812f4767efe43a974112950836577
Naige30 <ishigamisenku0603@gmail.com>
2026-03-10 01:25:55 +0800

    9
    9

---
+++ StoreModels/StoreModels.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StoreModels
+{
+    public class Store
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public double Profit { get; set; }
+        public double Expenses { get; set; }
+        public int Employees { get; set; }
+        public int Products { get; set; }
+    }
+}
